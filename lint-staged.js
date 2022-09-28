c349b29d1b2f066380bce0d40d94562e944e4e20
Victor Miranda <victormiranda13v@gmail.com>
2022-09-28 16:15:39 -0300

    chore: test

--- lint-staged.js
+++ lint-staged.js
@@ -2,7 +2,7 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": [
     "eslint --max-warnings=0",
-    "yarn test --bail --watchAll=false --findRelatedTests --passWithNoTests",
+    "react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests",
     () => "tsc-files --noEmit",
   ],
   "*.{js,jsx,ts,tsx,json,css,js}": ["prettier --write"],
